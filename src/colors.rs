ce61f27bcc85a99acc8d65daa136e4eed78c631b
Ivan Latunov <xswordsx@gmail.com>
2019-05-05 01:10:04 +0300

    Initial implementattion

---
+++ src/colors.rs
@@ -0,0 +1,98 @@
+// Wraps the str s into the passed down color. If the color
+// does not match any color - return it as-is.
+//
+// Takes into account the CLICOLOR, CLICOLOR_FORCE and NO_COLOR
+// variables.
+pub fn colorize(s: &str, color: &str) -> std::string::String {
+    if !should_color() {
+        return String::from(s);
+    }
+    match color {
+        "black" => return black(s),
+        "red" => return red(s),
+        "green" => return green(s),
+        "yellow" => return yellow(s),
+        "blue" => return blue(s),
+        "magenta" => return magenta(s),
+        "cyan" => return cyan(s),
+        "white" => return white(s),
+        "gray" => return gray(s),
+
+        "bold" => return bold(s),
+
+        _ => return String::from(s),
+    }
+}
+
+// Implements https://bixense.com/clicolors/
+fn clicolor() -> bool {
+    let force_color = match std::env::var("CLICOLOR_FORCE") {
+        Ok(val) => val != "0",
+        Err(_) => false,
+    };
+
+    if force_color {
+        return true;
+    }
+
+    if atty::is(atty::Stream::Stdout) {
+        match std::env::var("CLICOLOR") {
+            Ok(val) => return val != "0",
+            Err(_) => return true,
+        };
+    }
+
+    return false;
+}
+
+fn should_color() -> bool {
+    match std::env::var("NO_COLOR") {
+        // If set (regardless to what) -> don't color output.
+        Ok(_) => return false,
+
+        // Var is not present, bad unicode
+        Err(_) => return clicolor(),
+    }
+}
+
+// Foreground colors   // Background colors
+// 30 Black            // 40 Black
+// 31 Red              // 41 Red
+// 32 Green            // 42 Green
+// 33 Yellow           // 43 Yellow
+// 34 Blue             // 44 Blue
+// 35 Magenta          // 45 Magenta
+// 36 Cyan             // 46 Cyan
+// 37 White            // 47 White
+
+fn black(s: &str) -> std::string::String {
+    return format!("\x1B[30m{}\x1B[0m", s);
+}
+fn red(s: &str) -> std::string::String {
+    return format!("\x1B[31m{}\x1B[0m", s);
+}
+fn green(s: &str) -> std::string::String {
+    return format!("\x1B[32m{}\x1B[0m", s);
+}
+fn yellow(s: &str) -> std::string::String {
+    return format!("\x1B[33m{}\x1B[0m", s);
+}
+fn blue(s: &str) -> std::string::String {
+    return format!("\x1B[34m{}\x1B[0m", s);
+}
+fn magenta(s: &str) -> std::string::String {
+    return format!("\x1B[35m{}\x1B[0m", s);
+}
+fn cyan(s: &str) -> std::string::String {
+    return format!("\x1B[36m{}\x1B[0m", s);
+}
+fn white(s: &str) -> std::string::String {
+    return format!("\x1B[37m{}\x1B[0m", s);
+}
+
+fn gray(s: &str) -> std::string::String {
+    return format!("\x1B[90m{}\x1B[39m", s);
+}
+fn bold(s: &str) -> std::string::String {
+    return format!("\x1B[97m{}\x1B[0m", s);
+}
---
+++ src/main.rs
@@ -0,0 +1,86 @@
+mod colors;
+
+use chrono::DateTime;
+use std::io::{self, BufRead};
+use yaml_rust::{YamlEmitter, YamlLoader};
+
+fn format_line(mut obj: json::JsonValue) -> String {
+    let mut result = String::default();
+
+    // TODO: Support short version
+    if obj.has_key("time") {
+        let ts = obj["time"].as_str().unwrap();
+        let t = DateTime::parse_from_rfc3339(&ts).unwrap();
+        result.push_str(format!("{}", t.format("%H:%M:%S2%.3f")).as_str());
+    } else {
+        result.push_str("??:??:??.???");
+    }
+
+    if obj.has_key("severity") {
+        let svrty = severity_fmt(obj["severity"].as_str().unwrap());
+        result.push(' ');
+        result.push_str(svrty.as_str());
+    } else {
+        result.push_str(" [UNKNOWN]");
+    }
+
+    if obj.has_key("component") {
+        let svrty = severity_fmt(obj["component"].as_str().unwrap());
+        result.push(' ');
+        result.push_str(colors::colorize(svrty.as_str(), "cyan").as_str());
+    }
+
+    if obj.has_key("message") {
+        result.push(' ');
+        result.push_str(obj["message"].as_str().unwrap());
+    } else {
+        result.push_str(" <no message>");
+    }
+
+    obj.remove("time");
+    obj.remove("severity");
+    obj.remove("message");
+    obj.remove("component");
+
+    // Dump the YAML object
+    let mut out_str = String::new();
+    {
+        let doc = YamlLoader::load_from_str(obj.dump().as_str()).unwrap();
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(&doc[0]).unwrap(); // dump the YAML object to a String
+    }
+    if out_str != "---\n{}" && out_str != "---" && out_str != "" {
+        let formatted_data = out_str.trim_start_matches("---").replace('\n', "\n  ");
+        let colored = colors::colorize(formatted_data.as_str(), "gray");
+        result.push_str(colored.as_str());
+    }
+
+    return result;
+}
+
+fn severity_fmt(s: &str) -> String {
+    // TODO: Support short version
+    match s {
+        "debug" => return format!("[{}]", colors::colorize("DEBUG", "bold")),
+        "info" => return format!("[{} ]", colors::colorize("INFO", "green")),
+        "warn" => return format!("[{} ]", colors::colorize("WARN", "yellow")),
+        "error" => return format!("[{}]", colors::colorize("ERROR", "red")),
+        "fatal" => return format!("[{}]", colors::colorize("FATAL", "bold")),
+
+        _ => return String::from(s),
+    }
+}
+
+fn main() {
+    // TODO: Support --short & --version & --help flags
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let raw_line = line.unwrap();
+        let parsed_line = json::parse(&raw_line).unwrap_or(json::object! {});
+        if parsed_line == json::object! {} {
+            println!("{}", &raw_line);
+            continue;
+        }
+        println!("{}", format_line(parsed_line));
+    }
+}
